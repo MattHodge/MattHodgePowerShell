713046770f01facdb333336773a07f7062f6d76a
Matt Hodge <matthodge@gmail.com>
2016-01-23 16:58:10 -0600

    adding Get-ServiceHubot function

---
+++ Hubot/Get-ServiceHubot.ps1
@@ -0,0 +1,34 @@
+﻿<#
+.Synopsis
+    Gets service status for Hubot Script.
+.DESCRIPTION
+    Gets service status for Hubot Script.
+.EXAMPLE
+    Get-ServiceHubot -Name dhcp
+#>
+function Get-ServiceHubot
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Name of the Service
+        [Parameter(Mandatory=$true)]
+        $Name
+    )
+
+    $result = @{}
+                
+    try
+    {
+        $service = Get-Service -Name $Name -ErrorAction Stop
+        $result.output = "Service $($service.Name) (*$($service.DisplayName)*) is currently ``$($service.Status.ToString())``."
+        $result.success = $true
+    }
+    catch
+    {
+        $result.output = "Service $($Name) does not exist on this server."
+        $result.success = $false
+    }
+
+    return $result | ConvertTo-Json
+}
